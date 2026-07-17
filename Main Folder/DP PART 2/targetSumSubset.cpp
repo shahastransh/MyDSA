3fae56b2467856a89cd9fdf8b546aeafce02a714
shahastransh <shahastranshtiwari@gmail.com>
2026-07-17 17:47:32 +0530

    memoization

--- Main Folder/DP PART 2/targetSumSubset.cpp
+++ Main Folder/DP PART 2/targetSumSubset.cpp
@@ -28,10 +28,26 @@ bool targetSum(vector<int> nums, int target){
 
 }
 
+bool targetSum2(vector<vector<int>>&dp, vector<int> nums, int target, int n){
+    if(target == 0) return true;
+    if(n == 0) return false;
+
+    if(dp[n][target] != -1) return dp[n][target];
+
+    if(nums[n-1] <= target){
+        return dp[n][target] = targetSum2(dp, nums, target - nums[n-1], n-1) || targetSum2(dp, nums, target, n-1);
+    }else{
+        return dp[n][target] = targetSum2(dp, nums, target, n-1);
+    }
+
+}
+
 
 int main(){
     vector<int> nums = {4,2,7,1,3};
     int target = 10;
     cout << targetSum(nums,target);
+    vector<vector<int>>dp(nums.size()+1,vector<int>(target+1,-1));
+    cout << targetSum2(dp, nums, target, nums.size());
     return 0;
 }
